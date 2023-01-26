deeeec073cbc9e361dd650ab52fb80600443ccd6
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-01-26 10:04:24 +0000

    remove IDaimondBase interface

--- src/proxy/IProxy.sol
+++ src/proxy/IProxy.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.8;
 
 import {ISafeOwnable} from "@solidstate/contracts/access/ownable/ISafeOwnable.sol";
 import {IERC165} from "@solidstate/contracts/interfaces/IERC165.sol";
-import {IDiamondBase} from "@solidstate/contracts/proxy/diamond/base/IDiamondBase.sol";
 import {IDiamondReadable} from "@solidstate/contracts/proxy/diamond/readable/IDiamondReadable.sol";
 
-interface IProxy is IDiamondBase, IDiamondReadable, ISafeOwnable, IERC165 {
+interface IProxy is IDiamondReadable, ISafeOwnable, IERC165 {
     error FunctionSelectorNotFound();
     error FunctionCallReverted();
 
