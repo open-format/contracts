ee8cc6d3af9135ec5ab51220dc67a2d02b89a92c
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-02-01 12:26:44 +0000

    add BatchMintMetadata logic

--- src/tokens/ERC721/ERC721Base.sol
+++ src/tokens/ERC721/ERC721Base.sol
@@ -4,13 +4,18 @@ pragma solidity ^0.8.16;
 import {IERC165} from "@solidstate/contracts/interfaces/IERC165.sol";
 import {IERC721} from "@solidstate/contracts/interfaces/IERC721.sol";
 import {IERC2981} from "@solidstate/contracts/interfaces/IERC2981.sol";
-
-import {ERC721AUpgradeable} from "@erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
 import {Ownable} from "@solidstate/contracts/access/ownable/Ownable.sol";
 import {ERC165BaseInternal} from "@solidstate/contracts/introspection/ERC165/base/ERC165BaseInternal.sol";
 import {ERC2981, ERC2981Storage} from "@solidstate/contracts/token/common/ERC2981/ERC2981.sol";
+import {UintUtils} from "@solidstate/contracts/utils/UintUtils.sol";
 
-abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165BaseInternal {
+import {ERC721AUpgradeable} from "@erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
+
+import {BatchMintMetadata} from "./batchMintMetadata/BatchMintMetadata.sol";
+
+abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165BaseInternal, BatchMintMetadata {
+    mapping(uint256 => string) private fullURI;
+
     function initialize(string memory _name, string memory _symbol, address _royaltyReciever, uint16 _royaltyBPS)
         public
         initializerERC721A
@@ -26,6 +31,28 @@ abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165Base
     }
 
     /*//////////////////////////////////////////////////////////////
+                        Overriden ERC721A logic
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     *  @notice         Returns the metadata URI for an NFT.
+     *  @dev            See `BatchMintMetadata` for handling of metadata in this contract.
+     *
+     *  @param _tokenId The tokenId of an NFT.
+     */
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        // tokenURI was stored using mintTo
+        string memory fullUriForToken = fullURI[_tokenId];
+        if (bytes(fullUriForToken).length > 0) {
+            return fullUriForToken;
+        }
+
+        // tokenURI was stored using batchMintTo
+        string memory batchUri = _getBaseURI(_tokenId);
+        return string.concat(batchUri, UintUtils.toString(_tokenId));
+    }
+
+    /*//////////////////////////////////////////////////////////////
                             Minting logic
     //////////////////////////////////////////////////////////////*/
 
@@ -34,9 +61,11 @@ abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165Base
      *  @dev             The logic in the `_canMint` function determines whether the caller is authorized to mint NFTs.
      *
      *  @param _to       The recipient of the NFT to mint.
+     *  @param _tokenURI The full metadata URI for the NFT minted.
      */
-    function mintTo(address _to) public virtual {
+    function mintTo(address _to, string memory _tokenURI) public virtual {
         require(_canMint(), "Not authorized to mint.");
+        _setTokenURI(_nextTokenId(), _tokenURI);
         _safeMint(_to, 1);
     }
 
@@ -46,10 +75,12 @@ abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165Base
      *
      *  @param _to       The recipient of the NFT to mint.
      *  @param _quantity The number of NFTs to mint.
+     *  @param _baseURI  The baseURI for the `n` number of NFTs minted. The metadata for each NFT is `baseURI/tokenId`
      */
 
-    function batchMintTo(address _to, uint256 _quantity) public virtual {
+    function batchMintTo(address _to, uint256 _quantity, string memory _baseURI) public virtual {
         require(_canMint(), "Not authorized to mint.");
+        _batchMintMetadata(_nextTokenId(), _quantity, _baseURI);
         _safeMint(_to, _quantity);
     }
 
@@ -70,6 +101,11 @@ abstract contract ERC721Base is ERC721AUpgradeable, Ownable, ERC2981, ERC165Base
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721AUpgradeable, IERC165) returns (bool) {
         return _supportsInterface(interfaceId);
+    }
+
+    function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal virtual {
+        require(bytes(fullURI[_tokenId]).length == 0, "URI already set");
+        fullURI[_tokenId] = _tokenURI;
     }
 
     /**
