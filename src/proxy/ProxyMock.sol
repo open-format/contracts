10bd29d58d62a980296f97ea3279c15901cba0e9
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-02-07 16:04:08 +0000

    update proxy mock

--- src/proxy/ProxyMock.sol
+++ src/proxy/ProxyMock.sol
@@ -7,7 +7,7 @@ contract ProxyMock is Proxy {
     /**
      * @dev override constructer that disablesInitilizers with one that calls innit
      */
-    constructor(address _registry) Proxy(false) {
-        innit(msg.sender, _registry);
+    constructor(address _registry, address _globals) Proxy(false) {
+        innit(msg.sender, _registry, _globals);
     }
 }
