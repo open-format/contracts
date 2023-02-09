200976f557bc6b31a0b24ae14ec594b5898ea735
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-02-09 13:22:47 +0000

    fix interface path

--- src/extensions/defaultOperatorFilterer/DefaultOperatorFiltererConstants.sol
+++ src/extensions/defaultOperatorFilterer/DefaultOperatorFiltererConstants.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
-import {IOperatorFilterRegistry} from "../../../interface/IOperatorFilterRegistry.sol";
+import {IOperatorFilterRegistry} from "../../interface/IOperatorFilterRegistry.sol";
 
 IOperatorFilterRegistry constant OPERATOR_FILTER_REGISTRY =
     IOperatorFilterRegistry(0x000000000000AAeB6D7670E522A718067333cd4E);
