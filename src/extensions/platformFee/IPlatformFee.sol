dec20d214845077cf2f1cd0b4e37827b8f4d57ff
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-02-15 21:25:24 +0000

    refactor, add comments platformFee extension

--- src/extensions/platformFee/IPlatformFee.sol
+++ src/extensions/platformFee/IPlatformFee.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.16;
 
 interface IPlatformFee {
+    error Error_insufficientBalance();
+    error Error_insufficientValue();
+    error Error_unableToSendValue();
+
     event PaidPlatformFee(address currency, uint256 amount);
 }
