f41fb653dd66b8090186172c5e964f97fe9f2a9b
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-02-23 18:25:18 +0000

    add documentation

--- src/extensions/applicationFee/IApplicationFee.sol
+++ src/extensions/applicationFee/IApplicationFee.sol
@@ -9,4 +9,12 @@ interface IApplicationFee {
     error Error_currency_not_accepted();
 
     event PaidApplicationFee(address currency, uint256 amount);
+
+    /**
+     * @notice gets the application fee for a given price
+     * @param _price the given price of a transaction
+     * @return recipient the address to pay the application fee to
+     * @return amount the fee amount
+     */
+    function applicationFeeInfo(uint256 _price) external view returns (address recipient, uint256 amount);
 }
