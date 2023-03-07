7a081a70a471c699258d0838d62872dd289261c9
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-03-07 19:47:45 +0000

    refactor: clean up, add docs

--- src/extensions/ERC721Factory/ERC721Factory.sol
+++ src/extensions/ERC721Factory/ERC721Factory.sol
@@ -3,24 +3,46 @@ pragma solidity ^0.8.16;
 
 import {MinimalProxyFactory} from "@solidstate/contracts/factory/MinimalProxyFactory.sol";
 import {ReentrancyGuard} from "@solidstate/contracts/utils/ReentrancyGuard.sol";
-import {ERC721Base} from "../../tokens/ERC721/ERC721Base.sol";
 
 import {IERC721Factory} from "./IERC721Factory.sol";
 import {ERC721FactoryInternal} from "./ERC721FactoryInternal.sol";
 
 /**
+ * @dev ERC721 implementations must have an initialize function
+ */
+interface CompatibleERC721Implementation {
+    function initialize(
+        address _owner,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint16 _royaltyBps
+    ) external;
+}
+
+/**
  * @title   "ERC721Factory Extension"
- * @notice  (WIP) a factory contract for creating ECR721 contracts
- * @dev     deploys minimal proxys that point to ERC721Base implementation
+ * @notice  A factory contract extension for creating clones of ECR721 contracts
+ * @dev     deploys minimal proxy that point to ERC721 implementation
  *          compatible to be inherited by facet contract
  *          there is an internal dependency on the globals extension.
  * @dev     inheriting contracts must override the internal _canCreate function
  */
 
 abstract contract ERC721Factory is IERC721Factory, ERC721FactoryInternal, MinimalProxyFactory, ReentrancyGuard {
+    /**
+     * @notice creates an erc721 contract based on implementation
+     * @dev the hash of the name is used as a "salt" so each contract is deployed to a different address
+     *      the deployed contract is a minimal proxy that points to the implementation chosen
+     * @param _name the name of the ERC721 contract
+     * @param _symbol the symbol of the ERC721 contract
+     * @param _royaltyRecipient the default address that royalties are sent to
+     * @param _royaltyBps the default royalty percent in BPS
+     * @param _implementationId the chosen implementation of ERC721 contract
+     */
     function createERC721(
-        string memory _name,
-        string memory _symbol,
+        string calldata _name,
+        string calldata _symbol,
         address _royaltyRecipient,
         uint16 _royaltyBps,
         bytes32 _implementationId
@@ -34,8 +56,9 @@ abstract contract ERC721Factory is IERC721Factory, ERC721FactoryInternal, Minima
             revert("no implementation found");
         }
 
-        // TODO: WIP need to see other examples of factorys and handerling salt
+        // creating a salt based of the name enforces no name is the same on each app
         bytes32 salt = keccak256(abi.encode(_name));
+
         // check proxy has not deployed erc721 with the same name
         // deploying with the same salt would override that ERC721
         if (_getId(salt) != address(0)) {
@@ -45,14 +68,20 @@ abstract contract ERC721Factory is IERC721Factory, ERC721FactoryInternal, Minima
         // saves deployment for checking later
         _setId(salt, id);
 
-        // hook to add functionalty before create
+        // hook to add functionality before create
         _beforeCreate();
 
         // deploys new proxy using CREATE2
         id = _deployMinimalProxy(implementation, salt);
-        ERC721Base(payable(id)).initialize(msg.sender, _name, _symbol, _royaltyRecipient, _royaltyBps);
 
-        emit Created(id, msg.sender, _name, _symbol, _royaltyRecipient, _royaltyBps, _implementationId);
+        // initialize ERC721 contract
+        try CompatibleERC721Implementation(payable(id)).initialize(
+            msg.sender, _name, _symbol, _royaltyRecipient, _royaltyBps
+        ) {
+            emit Created(id, msg.sender, _name, _symbol, _royaltyRecipient, _royaltyBps, _implementationId);
+        } catch {
+            revert("failed to initialize");
+        }
     }
 
     function getERC721FactoryImplementation(bytes32 _implementationId) external view returns (address) {
