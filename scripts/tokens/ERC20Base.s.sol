5762f909c9d9883399fc45cae91e2267effe9ddd
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-03-17 10:00:08 +0000

    feat: add deploy scripts for tokens

---
+++ scripts/tokens/ERC20Base.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "forge-std/Script.sol";
+import {Utils} from "scripts/utils/Utils.sol";
+import {ERC20Base} from "src/tokens/ERC20/ERC20Base.sol";
+import {Globals} from "src/globals/Globals.sol";
+
+string constant contractName = "ERC20Base";
+bytes32 constant implementationId = "Base";
+
+contract Deploy is Script, Utils {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy
+        ERC20Base erc20base = new ERC20Base();
+
+        // add to globals
+        Globals(getContractDeploymentAddress("Globals")).setERC20Implementation(implementationId, address(erc20base));
+
+        vm.stopBroadcast();
+
+        exportContractDeployment(contractName, address(erc20base), block.number);
+    }
+}
---
+++ scripts/tokens/ERC721Base.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "forge-std/Script.sol";
+import {Utils} from "scripts/utils/Utils.sol";
+import {ERC721Base} from "src/tokens/ERC721/ERC721Base.sol";
+import {Globals} from "src/globals/Globals.sol";
+
+string constant CONTRACT_NAME = "ERC721Base";
+bytes32 constant implementationId = "Base";
+
+contract Deploy is Script, Utils {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy
+        ERC721Base erc721base = new ERC721Base();
+
+        // add to globals
+        Globals(getContractDeploymentAddress("Globals")).setERC721Implementation(implementationId, address(erc721base));
+
+        vm.stopBroadcast();
+
+        exportContractDeployment(CONTRACT_NAME, address(erc721base), block.number);
+    }
+}
