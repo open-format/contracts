980795591f6b871ffd22425977545aea7633968b
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-03-20 16:15:38 +0000

    feat: add creator access settings

--- src/facet/SettingsFacet.sol
+++ src/facet/SettingsFacet.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.16;
 
-import {SafeOwnable} from "@solidstate/contracts/access/ownable/SafeOwnable.sol";
+import {SafeOwnable, OwnableInternal} from "@solidstate/contracts/access/ownable/SafeOwnable.sol";
 import {ApplicationFee} from "../extensions/applicationFee/ApplicationFee.sol";
+import {ApplicationAccess, IApplicationAccess} from "../extensions/applicationAccess/ApplicationAccess.sol";
 
 /**
  * @title   "Settings Facet"
  * @notice  (WIP) allows app owner to manage application wide settings
  */
 
-contract SettingsFacet is ApplicationFee, SafeOwnable {
+contract SettingsFacet is ApplicationFee, SafeOwnable, ApplicationAccess {
     /**
      * @notice sets the application percentage fee in BPS and the recipient wallet
      * @param percentBPS The percentage used to calculate application fee
@@ -27,5 +28,31 @@ contract SettingsFacet is ApplicationFee, SafeOwnable {
      */
     function setAcceptedCurrencies(address[] memory currencies, bool[] memory approvals) external onlyOwner {
         _setAcceptedCurrencies(currencies, approvals);
+    }
+
+    /**
+     * @notice sets the approved accounts that have access to create new token contracts,
+     *         setting the zero address will open it up to all.
+     * @dev    the arrays of currencies and approvals must be the same length, emits CreatorAccessUpdated event
+     * @param  accounts the list of accounts to edit
+     * @param  approvals the list of approvals for the given accounts
+     */
+    function setCreatorAccess(address[] calldata accounts, bool[] calldata approvals) external onlyOwner {
+        _setCreatorAccess(accounts, approvals);
+    }
+
+    /**
+     * @notice checks if account has creator access for the application
+     * @param  account the list of accounts to edit
+     */
+    function hasCreatorAccess(address account) external view returns (bool) {
+        return _hasCreatorAccess(account);
+    }
+
+    /**
+     * @dev use safeOwnable for _transferOwnership instead of Ownable
+     */
+    function _transferOwnership(address account) internal override(OwnableInternal, SafeOwnable) {
+        SafeOwnable._transferOwnership(account);
     }
 }
