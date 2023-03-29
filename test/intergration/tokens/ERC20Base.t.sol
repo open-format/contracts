3a279747ceecb29a5223bd49b59b75afa7e522dd
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-03-29 10:17:15 +0100

    test: platform fee integration of ERC20 mintTo

---
+++ test/intergration/tokens/ERC20Base.t.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.16;
+
+// The following tests ERC20Base integration with Globals ERC20FactoryFacet and platform fees
+
+import "forge-std/Test.sol";
+
+import {
+    IDiamondWritable,
+    IDiamondWritableInternal
+} from "@solidstate/contracts/proxy/diamond/writable/IDiamondWritable.sol";
+
+import {Proxy} from "src/proxy/Proxy.sol";
+import {Upgradable} from "src/proxy/upgradable/Upgradable.sol";
+import {RegistryMock} from "src/registry/RegistryMock.sol";
+import {Factory} from "src/factory/Factory.sol";
+import {Globals} from "src/globals/Globals.sol";
+
+import {ERC20Base, ADMIN_ROLE, MINTER_ROLE} from "src/tokens/ERC20/ERC20Base.sol";
+import {IERC20Factory} from "@extensions/ERC20Factory/IERC20Factory.sol";
+import {ERC20FactoryFacet} from "src/facet/ERC20FactoryFacet.sol";
+import {SettingsFacet, IApplicationAccess} from "src/facet/SettingsFacet.sol";
+
+import {Deploy} from "scripts/core/Globals.s.sol";
+
+abstract contract Helpers {
+    function prepareSingleFacetCut(
+        address cutAddress,
+        IDiamondWritableInternal.FacetCutAction cutAction,
+        bytes4[] memory selectors
+    ) public pure returns (IDiamondWritableInternal.FacetCut[] memory) {
+        IDiamondWritableInternal.FacetCut[] memory cuts = new IDiamondWritableInternal.FacetCut[](1);
+        cuts[0] = IDiamondWritableInternal.FacetCut(cutAddress, cutAction, selectors);
+        return cuts;
+    }
+}
+
+contract Setup is Test, Helpers {
+    address creator;
+    address other;
+    address socialConscious;
+
+    Factory appFactory;
+    Proxy appImplementation;
+    Proxy app;
+    RegistryMock registry;
+    Globals globals;
+
+    SettingsFacet settingsFacet;
+
+    ERC20Base erc20Implementation;
+    bytes32 erc20ImplementationId;
+    ERC20FactoryFacet erc20FactoryFacet;
+
+    function setUp() public {
+        // assign addresses
+        creator = address(0x10);
+        other = address(0x11);
+        socialConscious = address(0x12);
+
+        vm.deal(creator, 1 ether);
+
+        // deploy contracts
+        globals = new Globals();
+        registry = new RegistryMock();
+        appImplementation = new  Proxy(true);
+        appFactory = new Factory(address(appImplementation), address(registry), address(globals));
+
+        erc20Implementation = new ERC20Base();
+        erc20ImplementationId = bytes32("Base");
+        erc20FactoryFacet = new ERC20FactoryFacet();
+
+        // create app
+        vm.prank(creator);
+        app = Proxy(payable(appFactory.create("platformFeeTest")));
+
+        // setup globals
+        globals.setPlatformFee(0, 0, socialConscious);
+        globals.setERC20Implementation(erc20ImplementationId, address(erc20Implementation));
+
+        settingsFacet = new SettingsFacet();
+        {
+            // add SettingsFacet to registry
+            bytes4[] memory selectors = new bytes4[](1);
+            selectors[0] = settingsFacet.setCreatorAccess.selector;
+
+            registry.diamondCut(
+                prepareSingleFacetCut(address(settingsFacet), IDiamondWritableInternal.FacetCutAction.ADD, selectors),
+                address(0),
+                ""
+            );
+        }
+
+        {
+            // add erc20FactoryFacet to registry
+            bytes4[] memory selectors = new bytes4[](3);
+            selectors[0] = erc20FactoryFacet.createERC20.selector;
+            selectors[1] = erc20FactoryFacet.getERC20FactoryImplementation.selector;
+            selectors[2] = erc20FactoryFacet.calculateERC20FactoryDeploymentAddress.selector;
+            registry.diamondCut(
+                prepareSingleFacetCut(
+                    address(erc20FactoryFacet), IDiamondWritableInternal.FacetCutAction.ADD, selectors
+                ),
+                address(0),
+                ""
+            );
+        }
+
+        _afterSetup();
+    }
+
+    /**
+     * @dev override to add more setup per test contract
+     */
+    function _afterSetup() internal virtual {}
+}
+
+contract ERC20Base_Setup is Setup {
+    ERC20Base base;
+
+    function _afterSetup() internal override {
+        // add lazy mint implementation
+        ERC20Base baseImplementation = new ERC20Base();
+        bytes32 baseImplementationId = bytes32("Base");
+        globals.setERC20Implementation(baseImplementationId, address(baseImplementation));
+
+        // create lazy mint erc20
+        vm.prank(creator);
+        // forgefmt: disable-start
+        base = ERC20Base(
+            ERC20FactoryFacet(address(app)).createERC20(
+                "name",
+                "symbol",
+                18,
+                0,
+                baseImplementationId
+            )
+        );
+        // forgefmt: disable-end
+    }
+}
+
+contract ERC20Base__integration_mintTo is ERC20Base_Setup {
+    function test_mints_nft() public {
+        vm.prank(creator);
+        base.mintTo(creator, 1000);
+
+        // check nft is minted to creator
+        assertEq(base.balanceOf(creator), 1000);
+    }
+
+    function test_pays_platform_fee() public {
+        // set platform base fee to 0.001 ether
+        uint256 basePlatformFee = 0.001 ether;
+        globals.setPlatformFee(basePlatformFee, 0, socialConscious);
+
+        vm.prank(creator);
+        base.mintTo{value: basePlatformFee}(creator, 1000);
+
+        // check platform fee has been received
+        assertEq(socialConscious.balance, basePlatformFee);
+    }
+}
