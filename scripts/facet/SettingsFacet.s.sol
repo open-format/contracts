73163f05ff6841d5d04bc8fa78cf710f2b0ce9a3
george-e-d-g-e <george@e-d-g-e.co.uk>
2023-03-30 09:35:51 +0100

    feat: create update script

--- scripts/facet/SettingsFacet.s.sol
+++ scripts/facet/SettingsFacet.s.sol
@@ -21,12 +21,14 @@ contract Deploy is Script, Utils {
         SettingsFacet settingsFacet = new SettingsFacet();
 
         // construct array of function selectors
-        bytes4[] memory selectors = new bytes4[](5);
+        bytes4[] memory selectors = new bytes4[](7);
         selectors[0] = settingsFacet.setApplicationFee.selector;
         selectors[1] = settingsFacet.setAcceptedCurrencies.selector;
         selectors[2] = settingsFacet.applicationFeeInfo.selector;
         selectors[3] = settingsFacet.setCreatorAccess.selector;
         selectors[4] = settingsFacet.hasCreatorAccess.selector;
+        selectors[5] = settingsFacet.platformFeeInfo.selector;
+        selectors[6] = settingsFacet.getGlobalsAddress.selector;
 
         // construct and ADD facet cut
         IDiamondWritableInternal.FacetCut[] memory cuts = new IDiamondWritableInternal.FacetCut[](1);
@@ -65,5 +67,45 @@ contract Patch is Script, Utils {
         // add to registry
         RegistryMock(payable(getContractDeploymentAddress("Registry"))).diamondCut(cuts, address(0), "");
         vm.stopBroadcast();
+    }
+}
+
+// expose globals - patch
+contract Update_ExposeGlobals is Script, Utils {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy new settings facet
+        SettingsFacet settingsFacet = new SettingsFacet();
+
+        // construct array of function selectors to replace
+        // keeping it neat having all selectors point to the latest deployment
+        bytes4[] memory replaceSelectors = new bytes4[](5);
+        replaceSelectors[0] = settingsFacet.setApplicationFee.selector;
+        replaceSelectors[1] = settingsFacet.setAcceptedCurrencies.selector;
+        replaceSelectors[2] = settingsFacet.applicationFeeInfo.selector;
+        replaceSelectors[3] = settingsFacet.setCreatorAccess.selector;
+        replaceSelectors[4] = settingsFacet.hasCreatorAccess.selector;
+
+        // add globals
+        bytes4[] memory addSelectors = new bytes4[](2);
+        addSelectors[0] = settingsFacet.platformFeeInfo.selector;
+        addSelectors[1] = settingsFacet.getGlobalsAddress.selector;
+
+        // construct REPLACE and ADD facet cuts
+        IDiamondWritableInternal.FacetCut[] memory cuts = new IDiamondWritableInternal.FacetCut[](2);
+        cuts[0] = IDiamondWritableInternal.FacetCut(
+            address(settingsFacet), IDiamondWritableInternal.FacetCutAction.REPLACE, replaceSelectors
+        );
+        cuts[1] = IDiamondWritableInternal.FacetCut(
+            address(settingsFacet), IDiamondWritableInternal.FacetCutAction.ADD, addSelectors
+        );
+
+        // update registry
+        RegistryMock(payable(getContractDeploymentAddress("Registry"))).diamondCut(cuts, address(0), "");
+        vm.stopBroadcast();
+
+        exportContractDeployment(CONTRACT_NAME, address(settingsFacet), block.number);
     }
 }
