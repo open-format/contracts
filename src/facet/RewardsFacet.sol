fc6d6791bfe01c9bcb7cb23f3981af044c2eee40
george-openformat <george@openformat.tech>
2024-05-20 11:50:22 +0100

    feat: update mintBadge, badgeMinted event + add batchMintBadge, ERC721Minted event

--- src/facet/RewardsFacet.sol
+++ src/facet/RewardsFacet.sol
@@ -35,7 +35,10 @@ bytes32 constant MINTER_ROLE = bytes32(uint256(1));
 contract RewardsFacet is Multicall, SafeOwnable {
     event TokenMinted(address token, address to, uint256 amount, bytes32 id, bytes32 activityType, string uri);
     event TokenTransferred(address token, address to, uint256 amount, bytes32 id, bytes32 activityType, string uri);
-    event BadgeMinted(address token, uint256 quantity, address to, bytes32 id, bytes32 activityType, string uri);
+    event ERC721Minted(address token, uint256 quantity, address to, bytes32 id, bytes32 activityType, string uri);
+    event BadgeMinted(
+        address token, uint256 quantity, address to, bytes32 activityId, bytes32 activityType, bytes data
+    );
     event BadgeTransferred(address token, address to, uint256 tokenId, bytes32 id, bytes32 activityType, string uri);
 
     error RewardsFacet_NotAuthorized();
@@ -69,6 +72,60 @@ contract RewardsFacet is Multicall, SafeOwnable {
         emit TokenTransferred(_token, _to, _amount, _id, _activityType, _uri);
     }
 
+    /**
+     * @notice  Mints a number of badges to an address and broadcasts activity metadata
+     * @dev     The sender must have a role of admin or minter on the badge contract.
+     *          The app being called must have a minter role on the badge contract.
+     *          The activity metadata is emitted as an event for indexing.
+     * @param _badgeContract The address of the ERC721 contract that represents all badges of the same type
+     * @param _to The address of the recipient of the badges
+     * @param _quantity The amount of badges to mint
+     * @param _activityId The id associated with this activity for example "beat the boss" or "collected 100 berries"
+     * @param _activityType The type of activity e.g "mission" or "action"
+     * @param _data Any other data that will be useful
+     */
+    function batchMintBadge(
+        address _badgeContract,
+        address _to,
+        uint256 _quantity,
+        bytes32 _activityId,
+        bytes32 _activityType,
+        bytes calldata _data
+    ) public {
+        if (!_canMint(_badgeContract)) {
+            revert RewardsFacet_NotAuthorized();
+        }
+
+        Badge(_badgeContract).batchMintTo(_to, _quantity);
+        emit BadgeMinted(_badgeContract, _quantity, _to, _activityId, _activityType, _data);
+    }
+
+    /**
+     * @notice  Mints one badge to an address and broadcasts activity metadata.
+     * @dev     The sender must have a role of admin or minter on the badge contract.
+     *          The app being called must have a minter role on the badge contract.
+     *          The activity metadata is emitted as an event for indexing.
+     * @param _badgeContract The address of the ERC721 contract that represents all badges of the same type
+     * @param _to The address of the recipient of the badge
+     * @param _activityId The id associated with this activity for example "beat the boss" or "collected 100 berries"
+     * @param _activityType The type of activity for example "mission" or "action"
+     * @param _data Any other data that will be useful
+     */
+    function mintBadge(
+        address _badgeContract,
+        address _to,
+        bytes32 _activityId,
+        bytes32 _activityType,
+        bytes calldata _data
+    ) public {
+        if (!_canMint(_badgeContract)) {
+            revert RewardsFacet_NotAuthorized();
+        }
+
+        Badge(_badgeContract).mintTo(_to);
+        emit BadgeMinted(_badgeContract, 1, _to, _activityId, _activityType, _data);
+    }
+
     function mintERC721(
         address _token,
         address _to,
@@ -81,25 +138,8 @@ contract RewardsFacet is Multicall, SafeOwnable {
         if (!_canMint(_token)) {
             revert RewardsFacet_NotAuthorized();
         }
-
         NFT(_token).batchMintTo(_to, _quantity, _baseURI);
-        emit BadgeMinted(_token, _quantity, _to, _id, _activityType, _uri);
-    }
-
-    function mintBadge(
-        address _token,
-        address _to,
-        uint256 _quantity,
-        bytes32 _id,
-        bytes32 _activityType,
-        string calldata _uri
-    ) public {
-        if (!_canMint(_token)) {
-            revert RewardsFacet_NotAuthorized();
-        }
-
-        Badge(_token).batchMintTo(_to, _quantity);
-        emit BadgeMinted(_token, _quantity, _to, _id, _activityType, _uri);
+        emit ERC721Minted(_token, _quantity, _to, _id, _activityType, _uri);
     }
 
     function transferERC721(
