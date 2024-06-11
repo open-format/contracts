af67de32524b56d01e37d01a37cf1ad4d65a1ef5
george-openformat <george@openformat.tech>
2024-06-11 12:58:31 +0100

    feat: remove operator filter

--- src/extensions/defaultOperatorFilterer/IDefaultOperatorFilterer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.16;
-
-interface IDefaultOperatorFilterer {
-    /// @dev Emitted when an operator is not allowed.
-    error DefaultOperatorFilterer_OperatorNotAllowed(address operator);
-}
